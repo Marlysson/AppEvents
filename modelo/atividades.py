b27bfa3af8185590ffcd6da1da63cd2a620fb439
Marlysson Silva <marlysson5@gmail.com>
2016-07-14 12:00:42 -0300

    Adicionando modelo de atividades dentro do evento. Servirá de uso tanto para evento como para a inscrição

---
+++ modelo/atividades.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*- 
+
+import sys,os
+
+# Adicionando pasta externa para capturar os modelos
+pasta_projeto = os.path.abspath("../")
+sys.path.append(pasta_projeto)
+
+from abstracoes.tipo_atividade import Atividade
+from enums.tipo_atividade import TipoAtividade
+
+class Palestra(Atividade):
+	def __init__(self,titulo):
+		super().__init__(titulo)
+		self.tipo = TipoAtividade.PALESTRA
+
+
+class Minicurso(Atividade):
+	def __init__(self,titulo):
+		super().__init__(titulo)
+		self.tipo = TipoAtividade.MINI_CURSO
+
+
+class Tutorial(Atividade):
+	def __init__(self,titulo):
+		super().__init__(titulo)
+		self.tipo = TipoAtividade.TUTORIAL	
