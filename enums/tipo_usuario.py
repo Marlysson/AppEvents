be1bbb0704f87afa38018ba4bf22da12901f1a05
Marlysson Silva <marlysson5@gmail.com>
2016-07-14 12:06:28 -0300

    Adicionando Enum para poder determinar o tipo de pessoa que está no evento.

---
+++ enums/tipo_usuario.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8
+
+from enum import Enum
+
+class TipoParticipante(Enum):
+	PARTICIPANTE  = "Participante"
+	ORGANIZADOR   = "Organizador"
+	PALESTRANTE   = "Palestrante"
+
+	def __str__(self):
+		return "{}".format(self.value)
