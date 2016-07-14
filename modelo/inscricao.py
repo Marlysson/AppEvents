83135aae7820d703e16b79253a53905144768e8b
Marlysson Silva <marlysson5@gmail.com>
2016-07-14 11:57:39 -0300

    Modelo de inscrição em um determinado evento.

---
+++ modelo/inscricao.py
@@ -0,0 +1,39 @@
+#-*- coding: utf-8 -*-
+
+from datetime import datetime
+
+class Inscricao(object):
+
+	def __init__(self,participante,evento):
+		self.participante   = participante
+		self.evento         = evento
+		self.atividades     = list()
+		self.cupons         = list()
+		self.data_pagamento = None
+
+		@property
+		def paga(self):
+			return ( self.data_pagamento - datetime.now() ).days < 0
+
+		@property
+		def preco(self):
+			resultado = 0.0
+
+			for atividade in atividades:
+				resultado += atividade.preco
+
+			# for cupon in cupons:
+			# 	cupon.descontar(self)
+
+			return resultado
+
+		def add_atividade(self,atividade):
+			self.atividades.append(atividade)
+
+		def add_cupon(self,cupom):
+			self.cupons.append(cupom)
+
+# insc = Inscricao(participante,evento)
+# insc.add_atividade(tutorial)
+# insc.add_atividade(minicurso)
+# insc.data_pagamento(datetime.now())
