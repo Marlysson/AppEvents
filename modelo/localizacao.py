edb0c9a6d0919a09f65cbacd285319a410fef0c1
Marlysson Silva <marlysson5@gmail.com>
2016-07-20 16:25:41 -0300

    Removendo atributo de Localização , pois não seria importante.

--- modelo/localizacao.py
+++ modelo/localizacao.py
@@ -1,11 +1,10 @@
 # -*- coding : utf-8 -*-
 
 class Localizacao(object):
-	def __init__(self,local,numero,cidade,estado):
-		self.local   = local
-		self.numero = numero
+	def __init__(self,local,cidade,estado):
+		self.local  = local
 		self.cidade = cidade
 		self.estado = estado
 
 	def __repr__(self):
-		return "{}, nº {}, {}-{}".format(self.local,self.numero,self.cidade,self.estado)
+		return "{}, {}-{}".format(self.local,self.cidade,self.estado)
