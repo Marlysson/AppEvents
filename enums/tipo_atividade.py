182d8fbb549e92fb5d067cb1de19aff6a92465a2
Marlysson Silva <marlysson5@gmail.com>
2016-07-20 14:13:08 -0300

    Refatorando entidade de Tipo de Atividade, desacoplando o preço das atividades pré-definidas do modelo relacionado, assim terá somente dados referentes ao tipo da atividade.

--- enums/tipo_atividade.py
+++ enums/tipo_atividade.py
@@ -3,13 +3,9 @@
 from enum import Enum
 
 class TipoAtividade(Enum):
-	PALESTRA     = ("Palestra",50)
-	TUTORIAL     = ("Tutorial",10)
-	MINI_CURSO   = ("Mini-Curso",60)
-
-	def __init__(self,nome,preco):
-		self.nome = nome
-		self.preco = float(preco)
+	PALESTRA     = "Palestra"
+	TUTORIAL     = "Tutorial"
+	MINI_CURSO   = "Mini-Curso"
 
 	def __str__(self):
-		return "{}({:.2f})".format(self.nome,self.preco)
+		return "{}".format(self.value)
