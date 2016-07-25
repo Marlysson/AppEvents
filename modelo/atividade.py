6d15f36e1be0d7ce3abad8fb37470f94730f3b83
Marlysson Silva <marlysson5@gmail.com>
2016-07-25 16:43:06 -0300

    Terminando classe de Atividade, colocando preço variável entre definido e grátis, e formatando representação.

--- modelo/atividade.py
+++ modelo/atividade.py
@@ -1,22 +1,11 @@
 
 class Atividade:
 
-	'''
-		>>> atividade = Atividade("Palestra","Iniciando Vue.js",40.00)
-		>>> print atividade
-		Iniciando Vue.js - Palestra
-	'''
-
-	def __init__(self,tipo,titulo,preco=None):
+	def __init__(self,tipo,titulo,horario,preco=None):
 		self.tipo = tipo
 		self.titulo = titulo
-		self.horario = None
-
-		if not preco:
-			self.preco = 0
-		else:
-			self.preco = preco
-			
+		self.horario = horario
+		self.preco = float(preco) if preco else 0
 		
 	def __repr__(self):
-		return "{} - {}".format(self.titulo,self.tipo)
+		return "<Atividade {} >".format(self.__dict__)
