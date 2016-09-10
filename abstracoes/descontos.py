5017fc26ae9daaa5d67986a799f25ee8c7706c9c
Marlysson Silva <marlysson5@gmail.com>
2016-09-10 18:43:37 -0300

    Adicionando regras de desconto e classe responsável por calcular o preço de uma inscrição.

---
+++ abstracoes/descontos.py
@@ -0,0 +1,61 @@
+# -*- coding : utf-8 -*-
+
+import sys,os
+
+# # Adicionando pasta externa para capturar os modelos
+diretorio_atual = os.getcwd()
+app = os.path.dirname(diretorio_atual)
+
+sys.path.append(app)
+
+from modelo.atividade import Atividade
+from abc import ABCMeta , abstractmethod
+
+class Desconto(metaclass=ABCMeta):
+
+	@abstractmethod
+	def obter_atividades(self,inscricao):
+		raise ValueError("Método 'obter_atividades' não implementada")
+
+
+class DescontoHackathon(Desconto):
+
+	def obter_atividades(self,inscricao):
+	    
+	    tipo_atividade = lambda atividade : atividade.tipo == "Hackathon"
+
+	    atividades = filter(tipo_atividade,inscricao.atividades)
+
+	    return atividades
+
+class DescontoWorkshop(Desconto):
+
+	def obter_atividades(self,inscricao):
+	    
+	    tipo_atividade = lambda atividade : atividade.tipo == "Workshop"
+
+	    atividades = filter(tipo_atividade,inscricao.atividades)
+
+	    return atividades
+	
+class DescontoTutorial(Desconto):
+
+	def obter_atividades(self,inscricao):
+	    tipo_atividade = lambda atividade : atividade.tipo == "Tutorial"
+
+	    atividades = filter(tipo_atividade,inscricao.atividades)
+
+	    return atividades
+
+class DescontoGeral(Desconto):
+
+	def obter_atividades(self,inscricao):
+	    return inscricao.atividades
+		
+class DescontoNulo(Desconto):
+
+	def obter_atividades(self,inscricao):
+
+		from datetime import datetime
+
+		return [ Atividade("Padrão","Padrão",datetime.now(),0.0) ]
---
+++ modelo/calculador_preco.py
@@ -0,0 +1,26 @@
+# -*- coding : utf-8 -*-
+
+import sys,os
+
+# Adicionando pasta externa para capturar os modelos
+diretorio_atual = os.getcwd()
+app = os.path.dirname(diretorio_atual)
+
+sys.path.append(app)
+
+from abstracoes.exceptions import CupomNaoEncontradoNoEvento
+from abstracoes.exceptions import CupomExpirado
+
+class CalculadorPreco(object):
+
+	def calcular_preco(self,inscricao):
+
+		cupom_usado = inscricao.cupom
+
+		valor_desconto = cupom_usado.obter_desconto(inscricao)
+
+		preco_total = inscricao.preco_total
+
+		preco_com_desconto = preco_total - valor_desconto
+		
+		return preco_com_desconto
