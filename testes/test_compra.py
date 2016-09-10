5e7bbf5ce5e55a065b8066eac3fdd7d34a039260
Marlysson Silva <marlysson5@gmail.com>
2016-09-10 20:44:47 -0300

    Adicionando alguns testes à classe responsável pela compra de uma inscrição no evento.

--- testes/test_compra.py
+++ testes/test_compra.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+#Bibliotecas padrão
 import unittest
 import sys,os
-from datetime import datetime, timedelta
+from datetime import datetime, date, timedelta
 
-# Adicionando pasta externa para capturar os modelos
+# Adicionando pasta externa para capturar os modelos externos
 diretorio_atual = os.getcwd()
 app = os.path.dirname(diretorio_atual)
 
@@ -15,108 +16,183 @@ from modelo.evento import Evento
 from modelo.inscricao import Inscricao
 from modelo.atividade import Atividade
 from modelo.pessoa import Pessoa
+from modelo.cupom import Cupom
+from modelo.compra import Compra
 
 #Enums
 from enums.tipo_sexo import TipoSexo
 from enums.tipo_atividade import TipoAtividade
 
-from abstracoes.regra_desconto import DescontoAtividade
+#Exceções
+from abstracoes.exceptions import ValorPagoInferior
+from abstracoes.exceptions import InscricaoJaPaga
+from abstracoes.exceptions import CupomExpirado
+from abstracoes.exceptions import CupomNaoEncontradoNoEvento
+
+#Descontos
+from abstracoes.descontos import DescontoTutorial , DescontoWorkshop , DescontoHackathon, DescontoGeral
 
 class TestCompraInscricao(unittest.TestCase):
 
 	def setUp(self):
 
-		hoje = datetime.now()
+		self.hoje = datetime.now()
+		self.data_cupons = date.today()
 
-		self.participante = Pessoa("Marcos",18,TipoSexo.MASCULINO)
+		inicio_evento = self.hoje + timedelta(days=1)
+		final_evento = inicio_evento + timedelta(days=2)
+		
+		#Horario Atividades
+		horario_tutorial = inicio_evento + timedelta(minutes=10)	
+		horario_palestra = horario_tutorial + timedelta(minutes=30)
+		horario_mesa_redonda = horario_palestra + timedelta(minutes=30)
+		horario_workshop = horario_mesa_redonda + timedelta(minutes=60)
+		horario_hackathon = horario_workshop + timedelta(minutes=60)
+		
+		#Participante
+		self.participante1 = Pessoa("Marlysson",18,TipoSexo.MASCULINO)
+		self.participante2 = Pessoa("Marcos",25,TipoSexo.MASCULINO)
 
-		horario_tutorial = datetime.now() + timedelta(1)
+		#Atividades
+		self.palestra     = Atividade(TipoAtividade.PALESTRA,"Lucrando com open-source",horario_palestra,0.0)
+		self.tutorial     = Atividade(TipoAtividade.TUTORIAL,"Boas Práticas com Django",horario_tutorial,15.00)
+		self.mesa_redonda = Atividade(TipoAtividade.MESA_REDONDA,"Pirataria na Web",horario_mesa_redonda,0.0)
+		self.hackathon    = Atividade(TipoAtividade.HACKATHON,"Soluções Financeiras - Nubank",horario_hackathon,50.00)
+		
+		self.workshop1     = Atividade(TipoAtividade.WORKSHOP,"Python na Ciência",horario_workshop,30.00)
+		self.workshop2     = Atividade(TipoAtividade.WORKSHOP,"Python no Ensino",horario_workshop,25.00)
 
-		horario_palestra = hoje + timedelta(days=1,minutes=40)
+		horarios_cupons = {
+			"valido1" :   self.data_cupons + timedelta(days=1),
+			"valido2" :   self.data_cupons + timedelta(days=1),
+			"invalido1" : self.data_cupons - timedelta(days=2),
+			"invalido2":  self.data_cupons - timedelta(days=2)
+		}
 
-		inicio_evento = hoje + timedelta(1)
-		final_evento = inicio_evento + timedelta(1)
+		self.cupom_valido1 = Cupom("LOTE_1",0.5,horarios_cupons.get("valido1"))
+		self.cupom_valido1.regra = DescontoGeral()
 
-		self.palestra = Atividade(TipoAtividade.PALESTRA,"Lucrando com open-source",horario_palestra,0.0)
-		self.tutorial = Atividade(TipoAtividade.TUTORIAL,"Boas Práticas com Django",horario_tutorial,15.00)
-
-		cupom1 = Cupom("MINICURSO_30",0.3,self.hoje - timedelta(1))
-		cupom1.promocao = DescontoAtividade(TipoAtividade.TUTORIAL)
+		self.cupom_valido2 = Cupom("TUTORIAIS_30",0.3,horarios_cupons.get("valido2"))
+		self.cupom_valido2.regra = DescontoTutorial()
+		
+		self.cupom_invalido1 = Cupom("WORKSHOP_10",0.1,horarios_cupons.get("invalido1"))
+		self.cupom_invalido1.regra = DescontoWorkshop()
+		
+		self.cupom_invalido2 = Cupom("HACKATHON_50",0.5,horarios_cupons.get("invalido2"))
+		self.cupom_invalido2.regra = DescontoHackathon()
 
 		self.evento = Evento("Software Freedom Day","Descrição do Evento",inicio_evento,final_evento)
-		
-		self.evento.adicionar_atividade(self.palestra)
-		self.evento.adicionar_atividade(self.tutorial)
-		
-	@unittest.skip("Não implementado")
-	def test_nao_deve_aplicar_descontos_de_cupons_nao_ativos(self):
-		pass
+		self.evento.prazo_inscricoes = date.today() + timedelta(days=3)
 
-	@unittest.skip("Não implementado")
-	def test_valor_total_das_inscricoes_e_o_valor_total_dos_seus_items(self):
-		pass
+		atividades = [self.palestra , self.tutorial , self.mesa_redonda , self.workshop1 , self.workshop2 , self.hackathon ]
 
-	# @unittest.skip("Não implementado")
+		for atividade in atividades:
+			self.evento.adicionar_atividade(atividade)
+
+		self.evento.adicionar_cupom(self.cupom_valido1)
+		self.evento.adicionar_cupom(self.cupom_valido2)
+		self.evento.adicionar_cupom(self.cupom_invalido1)
+
+
 	def test_inscricao_sem_itens_deve_ter_valor_zero(self):
 		
-		inscricao = Inscricao(self.participante,self.evento)
-
-		self.assertEqual(0,inscricao.preco_total)
-
-	@unittest.skip("Não implementado")
-	def test_ao_efetuar_compra_settar_inscricao_como_paga(self):
-		
-		inscricao = Inscricao(participante,evento)
-
-		inscricao.adicionar_atividade(tutorial)
+		inscricao = Inscricao(self.participante1,self.evento)
 
 		compra = Compra(inscricao)
 
+		self.assertEqual(0,compra.preco_total)
+
+	def test_valor_da_inscricao_e_o_valor_dos_seus_itens(self):
+		
+		inscricao = Inscricao(self.participante1,self.evento)
+
+		inscricao.adicionar_atividade(self.workshop1)
+		inscricao.adicionar_atividade(self.tutorial)
+
+		compra = Compra(inscricao)
+
+		valor_total = compra.preco_total
+
+		self.assertEqual(45.00,valor_total)
+		
+	def test_ao_efetuar_compra_settar_inscricao_como_paga(self):
+		
+		inscricao = Inscricao(self.participante2,self.evento)
+
+		inscricao.adicionar_atividade(self.tutorial)
+		inscricao.adicionar_atividade(self.workshop1)
+
+		compra = Compra(inscricao)
 		compra.pagar(150.00)
 
-	@unittest.skip("Não implementado")
+		self.assertTrue(inscricao.paga)
+
 	def test_deve_gerar_excecao_quando_pagar_uma_inscricao_ja_paga(self):
-		pass
-		
+
+		inscricao1 = Inscricao(self.participante2,self.evento)
+
+		inscricao1.adicionar_atividade(self.tutorial)
+		inscricao1.adicionar_atividade(self.workshop1)
+
+		compra1 = Compra(inscricao1)
+		compra1.pagar(150.00)
+
+		compra2 = Compra(inscricao1)
+
+		with self.assertRaises(InscricaoJaPaga):
+			compra2.pagar(100.00)
+			
 	@unittest.skip("Não implementado")
 	def test_deve_retornar_data_de_pagamento_correta_ao_efetuar_compra(self):
 		pass
 
-	@unittest.skip("Não implementado")
 	def test_nao_deve_aplicar_descontos_de_cupons_nao_ativos(self):
 		
-		inscricao = Inscricao(participante,evento)
+		inscricao = Inscricao(self.participante1,self.evento)
 
-		inscricao.adicionar_atividade(tutorial)
+		inscricao.adicionar_atividade(self.tutorial)
+		inscricao.adicionar_atividade(self.workshop1)
+		inscricao.adicionar_atividade(self.workshop2)
+		inscricao.adicionar_atividade(self.hackathon)
 
 		compra = Compra(inscricao)
-		# compra.adicionar_cupom(Cupom)
 
-		calculador = CalculadorDePreco(compra)
+		with self.assertRaises(CupomExpirado):
+			compra.aplicar_cupom(self.cupom_invalido1)
 
-		valor_a_pagar =	calculador.preco_total()
+	def test_deve_gerar_excecao_quando_aplicar_um_cupom_nao_cadastrado_no_evento(self):
+		
+		inscricao = Inscricao(self.participante2,self.evento)
 
-		compra.pagar(100)
+		inscricao.adicionar_atividade(self.tutorial)
+		inscricao.adicionar_atividade(self.workshop1)
+		inscricao.adicionar_atividade(self.workshop2)
+		inscricao.adicionar_atividade(self.hackathon)
 
-	@unittest.skip("Não implementado")
-	def test_valor_da_inscricao_e_o_valor_dos_seus_itens(self):
-		pass
+		compra = Compra(inscricao)
 
-	@unittest.skip("Não implementado")
+		with self.assertRaises(CupomNaoEncontradoNoEvento):
+			compra.aplicar_cupom(self.cupom_invalido2)
+
 	def test_deve_gerar_excecao_ao_realizar_pagamento_inferior_ao_valor(self):
-		pass
 
+		inscricao = Inscricao(self.participante1,self.evento)
+
+		inscricao.adicionar_atividade(self.tutorial)
+		inscricao.adicionar_atividade(self.workshop1)
+
+		compra = Compra(inscricao)
+
+		with self.assertRaises(ValorPagoInferior):
+			compra.pagar(40.00)
+			
 	@unittest.skip("Não implementado")
 	def test_inscricao_paga_nao_deve_aceitar_novos_itens(self):
 		pass
 
-
-# pagamento = Pagamento(inscricao,usuario)
-# pagamento.efetuar_pagamento(valor)
-
-# compra = Compra(inscricao,usuario)
-# compra.pagar(valor)
-
+	@unittest.skip("Não implementado")
+	def test_compra_de_uma_inscricao_com_cupom_deve_retornar_valor_atualizado(self):
+		pass
 
 if __name__ == "__main__":
 	unittest.main(verbosity=2)
